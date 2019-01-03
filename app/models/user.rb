343dd2ca4ab65550d48e4a50a997b4ae4fccd6ed
Lucas Rocha <ld.rocha@hotmail.com>
2019-01-03 17:45:48 -0200

    Installed and finished the devise setup

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
+  validates :first_name, presence: true, length: { minimum: 2 }, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :email, presence: true, uniqueness: true
+end
