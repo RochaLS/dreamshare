9b6bfa51c164ef46adc6a37c879eaa79f96d3d60
Lucas Rocha <ld.rocha@hotmail.com>
2019-01-05 12:57:51 -0200

    Finished basic pundit setup, and added an admin table to users

--- app/policies/dream_policy.rb
+++ app/policies/dream_policy.rb
@@ -4,4 +4,28 @@ class DreamPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def index?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+  end
+
+  def destroy?
+    user_is_owner_or_admin?
+  end
+
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
+  end
 end
