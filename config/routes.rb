2f61eba122ce6f9553f5a2857947615bbbe0224f
Lucas Rocha <ld.rocha@hotmail.com>
2019-01-05 17:18:20 -0200

    added a root for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  authenticated :user do
+    root 'dreams#index', as: :authenticated_root
+  end
+
   root to: 'pages#home'
 
   resources :dreams
