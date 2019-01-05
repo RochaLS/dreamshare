a3730a296b6ae28d1366590d999042569007fa81
Lucas Rocha <ld.rocha@hotmail.com>
2019-01-05 16:37:16 -0200

    Finished the backend of the profile page, and added some validations when creating a dream

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+    @users = policy_scope(User).order(created_at: :desc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+end
