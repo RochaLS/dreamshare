55dcdd395f99bec3d54d9c7059ea4b9296dfaffe
Lucas Rocha <ld.rocha@hotmail.com>
2019-01-16 20:13:51 -0200

    styled all the main forms

---
+++ app/javascript/components/remember_button.js
@@ -0,0 +1,11 @@
+// const r_button = document.getElementById('user_remember_me')
+
+// if(r_button) {
+//   const checkbox = document.querySelector('.remember_me_checkbox')
+//   checkbox.addEventListener('click', function(event) {
+//     r_button.click()
+//     event.checkbox.classList.add('.active')
+//   });
+// };
+
+
